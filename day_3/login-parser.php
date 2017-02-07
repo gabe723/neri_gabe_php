f7a8b26131bb80e905f5aa8e61fbaade7d091f67
gabe723 <gneri723@gmail.com>
2017-02-07 11:41:09 -0800

    login for day 3

---
+++ day_3/login-parser.php
@@ -0,0 +1,49 @@
+<?php
+//begin or resume the session
+session_start();
+//hide notices because they are ugly
+error_reporting( E_ALL & ~ E_NOTICE );
+//begin parsing the form if the user submitted it
+if( $_POST['submitted-login']){
+  //TEMPORARY: the correct credentials for logging in; TODO: replace with DB stuff later
+  $correct_username = 'gabe';
+  $correct_password = 'phprules1010';
+
+  //the values the user typed in:
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  //check to see if they matched both the UN and PW
+  if ( $username === $correct_username and $password === $correct_password ) {
+    //remember the user for one week
+    setcookie('logged-in-cookie', true, time() + 60 * 60 * 24 * 7 );
+    $_SESSION['logged-in-session'] = true;
+    //send to secret page
+    header('location:secret-page.php');
+  }else{
+    //show an error
+    $feedback = 'The information that was entered is incorrect. Please try again.';
+  }
+} //end of form parser
+
+//is the user trying to log out?
+if ( $_GET['action'] == 'logout' ) {
+  // Unset all of the session variables.
+  $_SESSION = array();
+
+  // If it's desired to kill the session, also delete the session cookie.
+  // Note: This will destroy the session, and not just the session data!
+  if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+    $params["path"], $params["domain"],
+    $params["secure"], $params["httponly"]
+  );
+}
+//destroy the session
+session_destroy();
+//force cookies to expire
+setcookie('logged-in-cookie', '', time() - 9999999);
+} //end of logout logic
+
+//do not close php when the document only contains php
---
+++ day_3/login.php
@@ -0,0 +1,34 @@
+<?php
+//require same as include except if the code fails the page is killed
+require('login-parser.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Login System</title>
+</head>
+<body>
+
+  <?php
+  //Lets you look at the post array (wrap in <pre> tag for better reading)
+  // print_r($_POST); ?>
+
+  <h1>Log In to your account fucker.</h1>
+
+  <?php echo $feedback; ?>
+
+  <form class="form-login" action="login.php" method="post">
+
+    <label for="login-username">Username:</label>
+    <input id="login-username" type="text" name="username" value="" autofocus required>
+
+    <label for="login-password">Password:</label>
+    <input id="login-password" type="password" name="password" value="" required>
+
+    <input type="submit" name="submit" value="Log In">
+    <input type="hidden" name="submitted-login" value="true">
+
+  </form>
+
+</body>
+</html>
